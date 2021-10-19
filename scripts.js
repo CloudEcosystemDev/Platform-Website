4c5bcf77e20361a3ef6de14ae18c9b8226e2c8c1
Ioannis Lafiotis <johnylaf94@gmail.com>
2021-10-19 10:56:52 +0200

    prettier

--- scripts.js
+++ scripts.js
@@ -1,18 +1,19 @@
 function accord(item) {
-var acc = document.getElementsByClassName("question");
-var i;
-for (i = 0; i < acc.length; i++) {
-        if(item === `${i}`){
+  var acc = document.getElementsByClassName("question");
+  var i;
+  for (i = 0; i < acc.length; i++) {
+    if (item === `${i}`) {
       var panel = document.getElementById(`p${i}`);
       if (panel.style.display === "block") {
-        console.log("clicked blocked")
+        console.log("clicked blocked");
 
         panel.style.display = "none";
       } else {
-        console.log("clicked no")
+        console.log("clicked no");
 
         panel.style.display = "block";
       }
-   } 
-// });
-  }}
+    }
+    // });
+  }
+}
