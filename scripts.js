340a90b1ab3113320b37bf8c454ed92ca18ef7f8
Ioannis Lafiotis <johnylaf94@gmail.com>
2021-10-19 14:38:23 +0200

    more spacing and small changes

--- scripts.js
+++ scripts.js
@@ -19,9 +19,11 @@ function accord(item) {
 function selectGif(item){
     item === 1 ? document.getElementById("dynImg").src="assets/chooseconnector.gif" : item === 2 ? document.getElementById("dynImg").src="assets/TemplateCreation.gif" : document.getElementById("dynImg").src="assets/IntegrationCenter.gif";
     let steps = document.getElementsByClassName("active-step");
-    console.log("steps",steps)
-    console.log("steps",steps.length)
-        steps[0].classList.remove("active-step");    
+    let vecs = document.getElementsByClassName("active-circle");
+
+    steps[0].classList.remove("active-step");    
+    vecs[0].classList.remove("active-circle");    
     document.getElementById(`s${item}`).classList.add("active-step");    
+    document.getElementById(`vec${item}`).classList.add("active-circle");    
 
 }
