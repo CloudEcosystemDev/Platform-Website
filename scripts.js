58eec86625a3caf40c5500b9f0f0f1abfdbec2c0
Ioannis Lafiotis <johnylaf94@gmail.com>
2021-10-19 11:39:42 +0200

    added the text for the questions

--- scripts.js
+++ scripts.js
@@ -3,10 +3,10 @@ function accord(item) {
   var i;
   for (i = 0; i < acc.length; i++) {
     if (item === `${i}`) {
-        var panel = document.getElementById(`p${i}`);
-        var vec = document.getElementById(`v${i}`);
-        if (panel.style.display === "block") { 
-        vec.style.transform = "rotate(0deg)";   
+      var panel = document.getElementById(`p${i}`);
+      var vec = document.getElementById(`v${i}`);
+      if (panel.style.display === "block") {
+        vec.style.transform = "rotate(0deg)";
         panel.style.display = "none";
       } else {
         vec.style.transform = "rotate(90deg)";
