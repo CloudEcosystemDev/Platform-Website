53fd57c4b844658477fb97ddecbbe0dd83038541
Ioannis Lafiotis <johnylaf94@gmail.com>
2021-10-19 14:52:19 +0200

    added the width for the pricing cards

--- scripts.js
+++ scripts.js
@@ -16,14 +16,17 @@ function accord(item) {
   }
 }
 
-function selectGif(item){
-    item === 1 ? document.getElementById("dynImg").src="assets/chooseconnector.gif" : item === 2 ? document.getElementById("dynImg").src="assets/TemplateCreation.gif" : document.getElementById("dynImg").src="assets/IntegrationCenter.gif";
-    let steps = document.getElementsByClassName("active-step");
-    let vecs = document.getElementsByClassName("active-circle");
+function selectGif(item) {
+  item === 1
+    ? (document.getElementById("dynImg").src = "assets/chooseconnector.gif")
+    : item === 2
+    ? (document.getElementById("dynImg").src = "assets/TemplateCreation.gif")
+    : (document.getElementById("dynImg").src = "assets/IntegrationCenter.gif");
+  let steps = document.getElementsByClassName("active-step");
+  let vecs = document.getElementsByClassName("active-circle");
 
-    steps[0].classList.remove("active-step");    
-    vecs[0].classList.remove("active-circle");    
-    document.getElementById(`s${item}`).classList.add("active-step");    
-    document.getElementById(`vec${item}`).classList.add("active-circle");    
-
+  steps[0].classList.remove("active-step");
+  vecs[0].classList.remove("active-circle");
+  document.getElementById(`s${item}`).classList.add("active-step");
+  document.getElementById(`vec${item}`).classList.add("active-circle");
 }
